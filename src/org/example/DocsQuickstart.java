222a1a3e7e6452d04191d7aaed58245cbb90f005
diegoAaguilar <diegoaguilar454@yahoo.com>
2022-04-25 12:06:40 -0400

    basic api creates document with a header and text

--- src/org/example/DocsQuickstart.java
+++ src/org/example/DocsQuickstart.java
@@ -12,14 +12,14 @@ import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.docs.v1.Docs;
 import com.google.api.services.docs.v1.DocsScopes;
-import com.google.api.services.docs.v1.model.Document;
+import com.google.api.services.docs.v1.model.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
+import java.util.*;
 import java.util.List;
 
 /* class to demonstarte use of Docs get documents API */
@@ -71,11 +71,49 @@ public class DocsQuickstart {
         Docs service = new Docs.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
-
         Document doc = new Document()
                 .setTitle("secret-hackermans-file");
         doc = service.documents().create(doc)
                 .execute();
+        String header = "Lorem Ipsum\n\n";
+        String text1 = "\tLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc aliquet bibendum enim facilisis gravida neque convallis a. Scelerisque purus semper eget duis at tellus. Eu mi bibendum neque egestas congue quisque egestas. Quam pellentesque nec nam aliquam sem et tortor consequat. Aliquam eleifend mi in nulla posuere. Amet mauris commodo quis imperdiet massa tincidunt nunc. Sit amet dictum sit amet justo donec. Sit amet porttitor eget dolor morbi. Pellentesque elit eget gravida cum sociis. Velit egestas dui id ornare. Praesent tristique magna sit amet purus. Augue lacus viverra vitae congue eu consequat ac felis. Sit amet volutpat consequat mauris nunc congue. Eget sit amet tellus cras adipiscing enim eu. Mi quis hendrerit dolor magna.\n\n";
+        String text2 = "\tOrnare arcu odio ut sem. Pretium lectus quam id leo in. Non consectetur a erat nam. Sit amet commodo nulla facilisi. Urna nunc id cursus metus aliquam. Venenatis urna cursus eget nunc scelerisque. Scelerisque in dictum non consectetur. Penatibus et magnis dis parturient montes nascetur ridiculus mus. Non blandit massa enim nec dui nunc. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Enim nunc faucibus a pellentesque sit amet porttitor eget dolor. Accumsan tortor posuere ac ut consequat semper. At erat pellentesque adipiscing commodo elit at imperdiet. A diam maecenas sed enim ut sem viverra aliquet eget. Amet justo donec enim diam vulputate. Dignissim suspendisse in est ante. Tellus mauris a diam maecenas sed.\n\n";
+        String text3 = "\tViverra nibh cras pulvinar mattis nunc sed blandit libero volutpat. A iaculis at erat pellentesque adipiscing. Scelerisque in dictum non consectetur a. Parturient montes nascetur ridiculus mus. Sit amet nisl purus in mollis nunc sed id semper. Semper auctor neque vitae tempus quam pellentesque nec. Adipiscing elit pellentesque habitant morbi tristique senectus et netus et. Ultricies mi eget mauris pharetra et ultrices neque ornare. Purus sit amet luctus venenatis lectus magna fringilla urna porttitor. Nulla pellentesque dignissim enim sit amet venenatis urna cursus. Mauris a diam maecenas sed enim ut sem viverra. Ullamcorper a lacus vestibulum sed arcu. Vestibulum morbi blandit cursus risus at ultrices.";
+        List<Request> requests = new ArrayList<>();
+        requests.add(new Request().setInsertText(new InsertTextRequest()
+                .setText(text1)
+                .setLocation(new Location().setIndex(1))));
+
+        requests.add(new Request().setInsertText(new InsertTextRequest()
+                .setText(text2)
+                .setLocation(new Location().setIndex(text1.length()+1))));
+
+        requests.add(new Request().setInsertText(new InsertTextRequest()
+                .setText(text3)
+                .setLocation(new Location().setIndex(text2.length()+text1.length()+1))));
+
+        requests.add(new Request().setInsertText(new InsertTextRequest()
+                .setText(header)
+                .setLocation(new Location().setIndex(1))));
+
+        requests.add(new Request().setUpdateParagraphStyle(new UpdateParagraphStyleRequest()
+                .setRange(new Range()
+                        .setStartIndex(1)
+                        .setEndIndex(header.length()+1))
+                .setParagraphStyle(new ParagraphStyle()
+                        .setNamedStyleType("HEADING_1")
+                        .setSpaceAbove(new Dimension()
+                                .setMagnitude(10.0)
+                                .setUnit("PT"))
+                        .setSpaceBelow(new Dimension()
+                                .setMagnitude(10.0)
+                                .setUnit("PT"))
+                        .setAlignment("CENTER"))
+                .setFields("namedStyleType,spaceAbove,spaceBelow,alignment")));
+
+        BatchUpdateDocumentRequest body = new BatchUpdateDocumentRequest().setRequests(requests);
+        BatchUpdateDocumentResponse response = service.documents()
+                .batchUpdate(doc.getDocumentId(), body).execute();
         System.out.println("Created document with title: " + doc.getTitle());
     }
 }
